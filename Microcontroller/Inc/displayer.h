d61e61fbebbaf995ef97f705a777ea666af642c6
Adam Przywuski <adamprzywuski98@gmail.com>
2019-05-07 13:56:41 +0200

    adding menu on lcd

---
+++ Microcontroller/Inc/displayer.h
@@ -0,0 +1,14 @@
+/*
+ * displayer.h
+ *
+ *  Created on: 03.05.2019
+ *      Author: adamp
+ */
+#include "i2c-lcd.h";
+
+#ifndef DISPLAYER_H_
+#define DISPLAYER_H_
+
+void menu(int num_of_messe);
+
+#endif /* DISPLAYER_H_ */
